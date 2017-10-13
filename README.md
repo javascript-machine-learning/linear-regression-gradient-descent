809c208465e4538fd8549266f702c979f9de320c
Robin Wieruch <wrobin@gmx.net>
2017-10-13 22:26:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Linear Regression with Gradient Descent
+
+This example project demonstrates how the [gradient descent](http://en.wikipedia.org/wiki/Gradient_descent) algorithm may be used to solve a [linear regression](http://en.wikipedia.org/wiki/Linear_regression) problem.
+
+The project showcases how a gradient descent may be used to solve the linear regression problem of fitting a line to a set of points in JavaScript. React is used as view later. In this problem, we wish to model a set of points using a line. The line model is defined by two parameters - the line's slope `m`, and y-intercept `b`. Gradient descent attemps to find the best values for these parameters, subject to a cost function.
+
+## Installation
+
+* `git clone git@github.com:rwieruch/linear-regression-gradient-descent.git`
+* `cd linear-regression-gradient-descent`
+* `npm install`
+* `npm start`
+* visit http://localhost:3000/
---
+++ src/App.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import './App.css';
+
+// adjust training set size
+
+const M = 5;
+
+// random training data preparation
+
+const DATA = [];
+
+const getRandomIntFromInterval = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const createRandomPortlandHouse = () => ({
+  squareMeter: getRandomIntFromInterval(0, 500),
+  price: getRandomIntFromInterval(0, 500),
+});
+
+for (let i = 0; i < M; i++) {
+  DATA.push(createRandomPortlandHouse());
+}
+
+const x = DATA.map(date => date.squareMeter);
+const y = DATA.map(date => date.price);
+
+// linear regression and gradient descent
+
+let m = 0;
+let b = 0;
+
+const hypothesis = x => m * x + b;
+
+const learn = (alpha) => {
+  if (x.length <= 0) return;
+
+  let sum1 = 0;
+  let sum2 = 0;
+
+  for (var i = 0; i < x.length; i++) {
+    sum1 += hypothesis(x[i]) - y[i];
+    sum2 += (hypothesis(x[i]) - y[i]) * x[i];
+  }
+
+  // b didn't change as much as m, so the learning rate is adjusted, anyone knows why?
+  b = b - 100000 * alpha * sum1 / (x.length);
+  m = m - alpha * sum2 / (x.length);
+}
+
+const cost = () => {
+  let sum = 0;
+
+  for (var i = 0; i < x.length; i++) {
+    sum += Math.pow(hypothesis(x[i]) - y[i], 2);
+  }
+
+  return sum / (2 * m);
+}
+
+// view
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      iteration: 0,
+    };
+
+    this.onLearn = this.onLearn.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.onLearn, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  onLearn() {
+    learn(0.000001);
+
+    this.setState(state => ({ iteration: state.iteration + 1 }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Plot
+          x={'squareMeter'}
+          y={'price'}
+        />
+
+        <div>
+          <Iteration iteration={this.state.iteration} />
+          <Hypothesis />
+          <Cost />
+        </div>
+      </div>
+    );
+  }
+}
+
+const Plot = ({ x, y }) =>
+  <svg width="500" height="500">
+    <line
+      x1="0"
+      y1={500 - hypothesis(0)}
+      x2="500"
+      y2={500 - hypothesis(500)}
+    />
+
+    {DATA.map((date, key) =>
+      <circle
+        key={key}
+        cx={date[x]}
+        cy={500 - date[y]}
+      />
+    )}
+  </svg>
+
+const Iteration = ({ iteration }) =>
+  <p>
+    <strong>Iteration:</strong> {iteration}
+  </p>
+
+const Hypothesis = () =>
+  <p>
+    <strong>Hypothesis:</strong> f(x) = {m.toFixed(2)}x + {b.toFixed(2)}
+  </p>
+
+const Cost = () =>
+  <p>
+    <strong>Cost:</strong> {cost().toFixed(2)}
+  </p>
+
+export default App;
